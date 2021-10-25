1ddc7ecc6b14f5a2753dd085851f3f1562f5b065
Ben Hallis <benhallis5@gmail.com>
2021-10-24 22:57:26 -0500

    Short Answer
    Added the ability to switch to short answer and back again.

--- create.js
+++ create.js
@@ -1,8 +1,124 @@
 document.getElementById("addCheck").addEventListener('click',addQuestion);
 let questionNum = 1;
 
+//Replaces question boxes with short answer box
+function replaceanswer(event){
+    
+    let num = event.substring(8);
+    let stringtonum = parseInt(num, 10);
+    let basenum = ((stringtonum-1)*4)+1;
+
+    let shortname = 'shortansbox' + stringtonum;
+    let optionsid = "options" + stringtonum;
+    
+    var shortansbox = document.createElement("textarea");
+    shortansbox.setAttribute("id", shortname);
+    shortansbox.setAttribute("class", "shortansbox");
+    shortansbox.setAttribute("rows", "5");
+    shortansbox.setAttribute("name", "shortansbox");
+    shortansbox.setAttribute("Placeholder", "Enter a rough summation of what the answer should be:");
+    document.getElementById(optionsid).replaceWith(shortansbox);    
+}
+
+//Replaces short answer box with multiple choice boxes
+function replaceshort(radioid){
+    let num = radioid.substring(10);
+    let stringtonum = parseInt(num, 10);
+    let basenum = ((stringtonum-1)*4)+1;
+    var linebreak = document.createElement("br");
+
+    let shortname = "shortansbox" + stringtonum;
+    //Finds what numbers the answers are throughout the entire quiz
+    let ans1 = "answer" + basenum;
+    let ans2 = "answer" + (basenum + 1);
+    let ans3 = "answer" + (basenum + 2);
+    let ans4 = "answer" + (basenum + 3);
+
+    let options = "options" + stringtonum;
+    let answers = "answers" + stringtonum;
+
+    var optionsdiv = document.createElement("div");
+    optionsdiv.setAttribute("class", "options");
+    optionsdiv.setAttribute("id", options);
+
+    var questionone = document.createElement("input");
+    questionone.setAttribute("type", "radio");
+    questionone.setAttribute("id", ans1);
+    questionone.setAttribute("name", answers);
+    questionone.setAttribute("value", "question1");
+    
+    var questiononelabel = document.createElement("input");
+    questiononelabel.setAttribute("type", "text");
+    questiononelabel.setAttribute("id", ans1);
+    questiononelabel.setAttribute("name", answers);
+    questiononelabel.setAttribute("placeholder", "Answer 1");
+              
+    var questiontwo = document.createElement("input");
+    questiontwo.setAttribute("type", "radio");
+    questiontwo.setAttribute("id", ans2);
+    questiontwo.setAttribute("name", answers);
+    questiontwo.setAttribute("value", "question2");
+    
+    var questiontwolabel = document.createElement("input");
+    questiontwolabel.setAttribute("type", "text");
+    questiontwolabel.setAttribute("name", answers);
+    questiontwolabel.setAttribute("id", ans2);
+    questiontwolabel.setAttribute("placeholder", "Answer 2");
+    
+    var questionthree = document.createElement("input");
+    questionthree.setAttribute("type", "radio");
+    questionthree.setAttribute("id", ans3);
+    questionthree.setAttribute("name", answers);
+    questionthree.setAttribute("value", "question3");
+    
+    var questionthreelabel = document.createElement("input");
+    questionthreelabel.setAttribute("type", "text");
+    questionthreelabel.setAttribute("id", ans3);
+    questionthreelabel.setAttribute("name", answers);
+    questionthreelabel.setAttribute("placeholder", "Answer 3");
+    
+    var questionfour = document.createElement("input");
+    questionfour.setAttribute("type", "radio");
+    questionfour.setAttribute("id", ans4);
+    questionfour.setAttribute("name", answers);
+    questionfour.setAttribute("value", "question4");
+    
+    var questionfourlabel = document.createElement("input");
+    questionfourlabel.setAttribute("type", "text");
+    questionfourlabel.setAttribute("id", ans4);
+    questionfourlabel.setAttribute("name", answers);
+    questionfourlabel.setAttribute("placeholder", "Answer 4");
+    
+    //Append everything
+
+    optionsdiv.appendChild(questionone);
+    optionsdiv.appendChild(questiononelabel);
+    optionsdiv.appendChild(linebreak);
+    linebreak = document.createElement("br");
+    
+    optionsdiv.appendChild(questiontwo);
+    optionsdiv.appendChild(questiontwolabel);
+    optionsdiv.appendChild(linebreak);
+    linebreak = document.createElement("br");
+    
+    optionsdiv.appendChild(questionthree);
+    optionsdiv.appendChild(questionthreelabel);
+    optionsdiv.appendChild(linebreak);
+    linebreak = document.createElement("br");
+    
+    optionsdiv.appendChild(questionfour);
+    optionsdiv.appendChild(questionfourlabel);
+    optionsdiv.appendChild(linebreak);
+    linebreak = document.createElement("br");
+    
+    document.getElementById(shortname).replaceWith(optionsdiv);
+
+}
+
 function addQuestion(){ 
     questionNum++;
+    //let shortansbox = "shortansbox" + questionNum;
+    let options = 'options' + questionNum;
     let shortans = 'shortans' + questionNum;
     let selectmult = 'selectmult' + questionNum;
     let multchoice = 'multchoice' + questionNum;
@@ -11,10 +127,12 @@ function addQuestion(){
     let answers = 'answers' + questionNum;
     var linebreak = document.createElement("br");
 
-    //Creates the new divs
-    var newDiv = document.createElement("div");
-    newDiv.setAttribute("class", "question");
+    let answer1 = "answer" + (((questionNum - 1) * 4) + 1);
+    let answer2 = "answer" + (((questionNum - 1) * 4) + 2);
+    let answer3 = "answer" + (((questionNum - 1) * 4) + 3);
+    let answer4 = "answer" + (((questionNum - 1) * 4) + 4);
 
+    //Creates the new divs
     var questionsdiv = document.createElement("div");
     questionsdiv.setAttribute("class", "questions");
     questionsdiv.setAttribute("id", "questions");
@@ -24,7 +142,8 @@ function addQuestion(){
 
     var optionsdiv = document.createElement("div");
     optionsdiv.setAttribute("class", "options");
-    optionsdiv.setAttribute("id", "options");
+    optionsdiv.setAttribute("id", options);
+    optionsdiv.setAttribute("name", questionNum);
 
     //Appends the text to the top
     var choosetext = document.createElement("h2");
@@ -37,6 +156,7 @@ function addQuestion(){
     multradio.setAttribute("id", multchoice);
     multradio.setAttribute("name", qtype);
     multradio.setAttribute("value", multchoice);
+    multradio.setAttribute("onclick", "replaceshort(this.id)")
 
     var multlabel = document.createElement("label");
     multlabel.setAttribute("for", multchoice)
@@ -48,6 +168,7 @@ function addQuestion(){
     selectradio.setAttribute("id", selectmult);
     selectradio.setAttribute("name", qtype);
     selectradio.setAttribute("value", selectmult);
+    selectradio.setAttribute("onclick", "replaceshort(this.id)")
 
     var selectlabel = document.createElement("label");
     selectlabel.htmlFor = selectmult;
@@ -59,10 +180,13 @@ function addQuestion(){
     shortradio.setAttribute("id", shortans);
     shortradio.setAttribute("name", qtype);
     shortradio.setAttribute("value", shortans);
+    shortradio.setAttribute("class", "shortans");
+    shortradio.setAttribute("onclick", "replaceanswer(this.id)")
 
     var shortlabel = document.createElement("label");
     shortlabel.htmlFor = shortans;
     shortlabel.innerHTML = "Short Answer";
+    shortlabel.setAttribute("for", shortans);
 
     //Creates new description area
     var textarea = document.createElement("textarea");
@@ -74,51 +198,56 @@ function addQuestion(){
 
     //Probably going to have to change around id values when setting up database
     //Creates new question answer areas
-    
-
     var questionone = document.createElement("input");
     questionone.setAttribute("type", "radio");
-    questionone.setAttribute("id", "multchoice");
+    questionone.setAttribute("id", answer1);
     questionone.setAttribute("name", answers);
     questionone.setAttribute("value", "question1");
-
+    
     var questiononelabel = document.createElement("input");
     questiononelabel.setAttribute("type", "text");
+    questiononelabel.setAttribute("id", answer1);
     questiononelabel.setAttribute("name", answers);
     questiononelabel.setAttribute("placeholder", "Answer 1");
                   
     var questiontwo = document.createElement("input");
     questiontwo.setAttribute("type", "radio");
-    questiontwo.setAttribute("id", "multchoice");
+    questiontwo.setAttribute("id", answer2);
     questiontwo.setAttribute("name", answers);
     questiontwo.setAttribute("value", "question2");
 
     var questiontwolabel = document.createElement("input");
     questiontwolabel.setAttribute("type", "text");
     questiontwolabel.setAttribute("name", answers);
+    questiontwolabel.setAttribute("id", answer2);
     questiontwolabel.setAttribute("placeholder", "Answer 2");
 
     var questionthree = document.createElement("input");
     questionthree.setAttribute("type", "radio");
-    questionthree.setAttribute("id", "multchoice");
+    questionthree.setAttribute("id", answer3);
     questionthree.setAttribute("name", answers);
     questionthree.setAttribute("value", "question3");
 
     var questionthreelabel = document.createElement("input");
     questionthreelabel.setAttribute("type", "text");
+    questionthreelabel.setAttribute("id", answer3);
     questionthreelabel.setAttribute("name", answers);
     questionthreelabel.setAttribute("placeholder", "Answer 3");
 
     var questionfour = document.createElement("input");
     questionfour.setAttribute("type", "radio");
-    questionfour.setAttribute("id", "multchoice");
+    questionfour.setAttribute("id", answer4);
     questionfour.setAttribute("name", answers);
     questionfour.setAttribute("value", "question4");
 
     var questionfourlabel = document.createElement("input");
     questionfourlabel.setAttribute("type", "text");
+    questionfourlabel.setAttribute("id", answer4);
     questionfourlabel.setAttribute("name", answers);
     questionfourlabel.setAttribute("placeholder", "Answer 4");
+
+    var timerdiv = document.createElement("div");
+    timerdiv.setAttribute("class", "timer");
 
     var timerinvis = document.createElement("input");
     timerinvis.setAttribute("type", "radio");
@@ -148,51 +277,34 @@ function addQuestion(){
     questionsdiv.appendChild(linebreak);
     linebreak = document.createElement("br");
 
-    
-
-    newDiv.appendChild(questionone);
-    newDiv.appendChild(questiononelabel);
-    newDiv.appendChild(linebreak);
+    optionsdiv.appendChild(questionone);
+    optionsdiv.appendChild(questiononelabel);
+    optionsdiv.appendChild(linebreak);
     linebreak = document.createElement("br");
 
-    optionsdiv.appendChild(newDiv);
-    newDiv = document.createElement("div");
-    newDiv.setAttribute("class", "question");
-
-    newDiv.appendChild(questiontwo);
-    newDiv.appendChild(questiontwolabel);
-    newDiv.appendChild(linebreak);
+    optionsdiv.appendChild(questiontwo);
+    optionsdiv.appendChild(questiontwolabel);
+    optionsdiv.appendChild(linebreak);
     linebreak = document.createElement("br");
 
-    optionsdiv.appendChild(newDiv);
-    newDiv = document.createElement("div");
-    newDiv.setAttribute("class", "question");
-
-    newDiv.appendChild(questionthree);
-    newDiv.appendChild(questionthreelabel);
-    newDiv.appendChild(linebreak);
+    optionsdiv.appendChild(questionthree);
+    optionsdiv.appendChild(questionthreelabel);
+    optionsdiv.appendChild(linebreak);
     linebreak = document.createElement("br");
 
-    optionsdiv.appendChild(newDiv);
-    newDiv = document.createElement("div");
-    newDiv.setAttribute("class", "question");
-
-    newDiv.appendChild(questionfour);
-    newDiv.appendChild(questionfourlabel);
-    newDiv.appendChild(linebreak);
-    linebreak = document.createElement("br");
-
-    optionsdiv.appendChild(newDiv);
-    newDiv = document.createElement("div");
-    newDiv.setAttribute("class", "timer");
-
-    newDiv.appendChild(timerinvis);
-    newDiv.appendChild(timer);
-    newDiv.appendChild(linebreak);
+    optionsdiv.appendChild(questionfour);
+    optionsdiv.appendChild(questionfourlabel);
+    optionsdiv.appendChild(linebreak);
     linebreak = document.createElement("br");
 
     questionsdiv.appendChild(optionsdiv);
-    questionsdiv.appendChild(newDiv);
+
+    timerdiv.appendChild(timerinvis);
+    timerdiv.appendChild(timer);
+    timerdiv.appendChild(linebreak);
+    linebreak = document.createElement("br");
+
+    questionsdiv.appendChild(timerdiv);
 
     document.getElementById("addNew").appendChild(questionsdiv);
 
