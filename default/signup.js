508d60608705a1852ac578a6312fa6da145b55e2
Ben Hallis <benhallis5@gmail.com>
2021-11-03 18:21:33 -0500

    Added sign out button, pop ups for login, create account

---
+++ default/signup.js
@@ -0,0 +1,6 @@
+document.getElementById("enter").addEventListener("click", valid);
+
+function valid(){
+    alert("You have successfully made an account. You can now log in.");
+    window.open('login.html', '_self');
+}
