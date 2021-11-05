2ab2ea8f73367ebe4f2115bac52de0dc379c721e
ChaseSylla <chasesylla@gmail.com>
2021-11-05 11:14:30 -0500

    Created popup for when quiz is created with code

--- student/logout.js
+++ student/logout.js
@@ -1,7 +1,6 @@
 document.getElementById("out").addEventListener("click", signout);
 
 function signout(){
-    
     if(confirm("Would you like to sign out?")){
         window.open('/default/home.html', '_self');
     }
