0bb72d2260e34be3303e0f5091c10763ed3350e1
CaseyChadwick <72931585+CaseyChadwick@users.noreply.github.com>
2021-11-28 19:52:46 -0600

    Add files via upload

---
+++ default/connect.php
@@ -0,0 +1,15 @@
+<?php
+	DEFINE ('DB_USER', 'root');
+	DEFINE ('DB_PASSWORD', '');
+	DEFINE ('DB_HOST', 'localhost');
+	DEFINE ('DB_NAME', 'quiz_management');
+
+	$con = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
+	OR die('Could not connect to MySQL: ' .
+	mysqli_connect_error());
+	
+	
+	if(!mysqli_select_db($con, 'quiz_management')){
+		echo 'Database not selected';
+	}
+?>
