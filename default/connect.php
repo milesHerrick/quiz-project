a200c1778293d9decdc5e3657ccedf6097b6a647
CaseyChadwick <72931585+CaseyChadwick@users.noreply.github.com>
2021-12-02 20:17:57 -0600

    connected to the remote database

--- default/connect.php
+++ default/connect.php
@@ -1,15 +1,15 @@
 <?php
-	DEFINE ('DB_USER', 'root');
-	DEFINE ('DB_PASSWORD', '');
-	DEFINE ('DB_HOST', 'localhost');
-	DEFINE ('DB_NAME', 'quiz_management');
+	DEFINE ('DB_USER', 'CS330G3');
+	DEFINE ('DB_PASSWORD', '6MISP7KE');
+	DEFINE ('DB_HOST', 'alfred.cs.uwec.edu');
+	DEFINE ('DB_NAME', 'CS330_GROUP3');
 
 	$con = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
 	OR die('Could not connect to MySQL: ' .
 	mysqli_connect_error());
 	
 	
-	if(!mysqli_select_db($con, 'quiz_management')){
+	if(!mysqli_select_db($con, 'CS330_GROUP3')){
 		echo 'Database not selected';
 	}
 ?>
