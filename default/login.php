c1b4b1f49190f2de5bbd82022e483528819184a0
CaseyChadwick <72931585+CaseyChadwick@users.noreply.github.com>
2021-12-06 11:12:50 -0600

    Update login.php

--- default/login.php
+++ default/login.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Log In</title>
     <link rel="stylesheet" href="login.css">
-    <link rel="stylesheet" href="/global.css">
+    <link rel="stylesheet" href="../global.css">
 </head>
 <body>
     <div>
